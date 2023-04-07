cf39097c6e69de6beff73ed55ca23b96318c5901
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-04-07 14:18:29 -0400

    Create characterDisplay.js

---
+++ marvel-character-search/components/characterDisplay.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function characterDisplay(data) {
+  return (
+    <>
+      {/*TODO: create function that accomplishes:
+      if data has information (is true) set display to visible, else set display to none  */}
+
+      {/* TODO: if data contains more than one character show each character name for user
+      to select which character they want, else if only one character is in data show character picture and bio */}
+
+      {/* TODO: per Marvel API the line "Data provided by Marvel. © 2014 Marvel" must be added along with a link provided by 
+      URL arrays or "http://marvel.com"  */}
+    </>
+  );
+}
+
+export default characterDisplay
