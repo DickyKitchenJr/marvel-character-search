9d1715f26103c3abaf40ded929a33d075a682386
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-04-17 12:54:11 -0400

    Update next.config.js

--- marvel-character-search/next.config.js
+++ marvel-character-search/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "i.annihil.us",
+        port: '',
+      },
+    ],
+  },
+};
+// TODO: Image not loading, see error when calling character on page; research and resolve this issue
 module.exports = nextConfig
