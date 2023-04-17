3ee7cfe8ed9e2cf201c32b9b34325fd1cf7abd5f
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-04-17 12:54:08 -0400

    Update characterDisplay.js

--- marvel-character-search/components/characterDisplay.js
+++ marvel-character-search/components/characterDisplay.js
@@ -1,15 +1,37 @@
 import React from "react";
+import Image from "next/image";
 
 function CharacterDisplay({ data }) {
-  
   return (
     <>
-      <p>Here I Am!</p>
-    
+    {console.log(`current credit is ${data.credit}`)}
+    {console.log(data)}
       {/*TODO: create function that accomplishes:
       if data has information (is true) set display to visible, else set display to none  */}
-      
-      {!data[0] ? <h2>No Data</h2> : <h2>Data Recieved</h2>}
+
+      {!data[0] ? null : data[0] === "No Character Found" ? (
+        <div>
+          <h2>No Character Found</h2>
+        <p>This may be due to a mispelling or Marvel.com (which provides the data used here) may not be updated with the current information for this character.</p>
+        </div>
+      ) : data.length === 1 ? (
+        <div key={data[0].id}>
+          <h2> One Character Found</h2>
+          <h3>{data[0].name}</h3>
+        </div>
+        
+      ) : (
+        <div>
+          <h2>Multiple Characters Found</h2>
+          {data.map((items) => {
+        return <div key={items.id}><h3>{items.name}</h3>
+        <Image src={items.image} alt={items.name} width={168} height={252}/>
+        {!items.description ? <h3>No Bio Found</h3> : <div><h3>Bio:</h3><p>{items.description}</p></div>}
+        {data.credit}    
+        </div>
+      })}
+        </div>
+      )}
       
       {/* TODO: if data contains more than one character show each character name for user
       to select which character they want, else if only one character is in data show character picture and bio */}
