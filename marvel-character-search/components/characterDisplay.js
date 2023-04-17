55d1b0c7c88054ca9db72be11a273aaa6bd07f1f
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-04-17 14:04:58 -0400

    Update characterDisplay.js

--- marvel-character-search/components/characterDisplay.js
+++ marvel-character-search/components/characterDisplay.js
@@ -4,40 +4,57 @@ import Image from "next/image";
 function CharacterDisplay({ data }) {
   return (
     <>
-    {console.log(`current credit is ${data.credit}`)}
-    {console.log(data)}
-      {/*TODO: create function that accomplishes:
-      if data has information (is true) set display to visible, else set display to none  */}
-
+    {/* PURPOSE: depending on contents of data, will show either error message or character info requested by user */}
       {!data[0] ? null : data[0] === "No Character Found" ? (
         <div>
           <h2>No Character Found</h2>
-        <p>This may be due to a mispelling or Marvel.com (which provides the data used here) may not be updated with the current information for this character.</p>
+          <p>
+            This may be due to a mispelling or Marvel.com (which provides the
+            data used here) may not be updated with the current information for
+            this character.
+          </p>
         </div>
       ) : data.length === 1 ? (
         <div key={data[0].id}>
           <h2> One Character Found</h2>
           <h3>{data[0].name}</h3>
+          <Image src={data[0].image} alt={data[0].name} width={168} height={252} />
+          {!data[0].description ? <h3>No Bio Found: Marvel.com (which provides the data used here)
+                    may not be updated with the current information for this
+                    character.</h3> : <div><h3>Bio:</h3> <p>{data[0].description}</p></div>}
+          <a href="https://marvel.com">{data.credit}</a>
         </div>
-        
       ) : (
         <div>
           <h2>Multiple Characters Found</h2>
           {data.map((items) => {
-        return <div key={items.id}><h3>{items.name}</h3>
-        <Image src={items.image} alt={items.name} width={168} height={252}/>
-        {!items.description ? <h3>No Bio Found</h3> : <div><h3>Bio:</h3><p>{items.description}</p></div>}
-        {data.credit}    
-        </div>
-      })}
+            return (
+              <div key={items.id}>
+                <h3>{items.name}</h3>
+                <Image
+                  src={items.image}
+                  alt={items.name}
+                  width={168}
+                  height={252}
+                />
+                {!items.description ? (
+                  <h3>
+                    No Bio Found: Marvel.com (which provides the data used here)
+                    may not be updated with the current information for this
+                    character.
+                  </h3>
+                ) : (
+                  <div>
+                    <h3>Bio:</h3>
+                    <p>{items.description}</p>
+                  </div>
+                )}
+                <a href="https://marvel.com">{data.credit}</a>
+              </div>
+            );
+          })}
         </div>
       )}
-      
-      {/* TODO: if data contains more than one character show each character name for user
-      to select which character they want, else if only one character is in data show character picture and bio */}
-
-      {/* TODO: per Marvel API the line "Data provided by Marvel. © 2014 Marvel" must be added along with a link provided by 
-      URL arrays or "http://marvel.com"  */}
     </>
   );
 }
