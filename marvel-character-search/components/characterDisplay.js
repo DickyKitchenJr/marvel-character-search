2b300d80c6c97d7cc23a1b5557622e059c8cbd8f
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-04-18 10:19:27 -0400

    Update characterDisplay.js

--- marvel-character-search/components/characterDisplay.js
+++ marvel-character-search/components/characterDisplay.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 function CharacterDisplay({ data }) {
   return (
     <>
-    {/* PURPOSE: depending on contents of data, will show either error message or character info requested by user */}
+      {/* PURPOSE: depending on contents of data, will show either error message or character info requested by user */}
       {!data[0] ? null : data[0] === "No Character Found" ? (
         <div>
           <h2>No Character Found</h2>
@@ -18,10 +18,22 @@ function CharacterDisplay({ data }) {
         <div key={data[0].id}>
           <h2> One Character Found</h2>
           <h3>{data[0].name}</h3>
-          <Image src={data[0].image} alt={data[0].name} width={168} height={252} />
-          {!data[0].description ? <h3>No Bio Found: Marvel.com (which provides the data used here)
-                    may not be updated with the current information for this
-                    character.</h3> : <div><h3>Bio:</h3> <p>{data[0].description}</p></div>}
+          <Image
+            src={data[0].image}
+            alt={data[0].name}
+            width={168}
+            height={252}
+          />
+          {!data[0].description ? (
+            <h3>
+              No Bio Found: Marvel.com (which provides the data used here) may
+              not be updated with the current information for this character.
+            </h3>
+          ) : (
+            <div>
+              <h3>Bio:</h3> <p>{data[0].description}</p>
+            </div>
+          )}
           <a href="https://marvel.com">{data.credit}</a>
         </div>
       ) : (
@@ -49,10 +61,10 @@ function CharacterDisplay({ data }) {
                     <p>{items.description}</p>
                   </div>
                 )}
-                <a href="https://marvel.com">{data.credit}</a>
               </div>
             );
           })}
+          <a href="https://marvel.com">{data.credit}</a>
         </div>
       )}
     </>
